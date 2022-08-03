e5589fd8bb4f4e1b14774940aa0ef9fd75181f86
James Harris <contact@jamesharris.id.au>
2022-08-03 10:53:07 +1000

    Add kubernetes examples.

---
+++ kubernetesexample_test.go
@@ -0,0 +1,43 @@
+package ferrite_test
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/dogmatiq/ferrite"
+)
+
+func ExampleKubeService() {
+	setUp()
+	defer tearDown()
+
+	value := ferrite.
+		KubeService("ferrite-svc")
+
+	os.Setenv("FERRITE_SVC_SERVICE_HOST", "host.example.org")
+	os.Setenv("FERRITE_SVC_SERVICE_PORT", "12345")
+	ferrite.ValidateEnvironment()
+
+	fmt.Println("address is", value.Address())
+
+	// Output:
+	// address is host.example.org:12345
+}
+
+func ExampleString_namedPort() {
+	setUp()
+	defer tearDown()
+
+	value := ferrite.
+		KubeService("ferrite-svc").
+		WithNamedPort("api")
+
+	os.Setenv("FERRITE_SVC_SERVICE_HOST", "host.example.org")
+	os.Setenv("FERRITE_SVC_SERVICE_PORT_API", "12345") // note _API suffix
+	ferrite.ValidateEnvironment()
+
+	fmt.Println("address is", value.Address())
+
+	// Output:
+	// address is host.example.org:12345
+}
