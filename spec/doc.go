96612be2f455c22941c1586c22b346679d0aa845
James Harris <contact@jamesharris.id.au>
2022-08-08 10:41:00 +1000

    Add package docs to `spec` package.

---
+++ spec/doc.go
@@ -0,0 +1,6 @@
+// Package spec contains utilities for building environment variable
+// specifications.
+//
+// It is used by Ferrite's built-in builders and may also be used to define
+// custom builders.
+package spec
