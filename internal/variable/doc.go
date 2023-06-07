bb023ba17f378ad5c2373ca6f12138208c2278a9
James Harris <contact@jamesharris.id.au>
2023-06-07 11:18:55 +1000

    Remove warning about internal package not being part of public API.

--- internal/variable/doc.go
+++ internal/variable/doc.go
@@ -1,7 +1,3 @@
 // Package variable contains utilities for describing and representing
 // environment variables.
-//
-// WARNING: This package is exposed to allow for experimentation but it is not
-// yet considered part of Ferrite's public API. It may change at any time and
-// without warning.
 package variable
